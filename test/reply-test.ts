984b46adda0061f2b99b8aab5154be25ac7e7ec2
Gene Hightower <gene@digilicious.com>
2021-03-13 01:09:04 -0800

    not working yet

--- test/reply-test.ts
+++ test/reply-test.ts
@@ -2,16 +2,34 @@
 
 const assert = require("assert");
 
-const { enc } = require("../lib/index.js");
-const { parse } = require("smtp-address-parse");
+const { encodeReply, decodeReply } = require("../lib/index");
+import type { FromTo } from "../lib/index";
+
+const { parse } = require("smtp-address-parser");
 
 const secret = "Not a real secret, of course.";
 
-describe("good addresses pass", function () {
-    it("a test", function () {
-        const address = "asdfsdf@duck.com";
-        const a = parse(address);
-        const s = enc.encodeReply(enc.decodeReply(a, secret), secret);
-        assert.equal(a.localPart.DotString, s);
+const testCases: FromTo[] = [
+    {"mailFrom": "reply@example.com", "rcptToLocalPart": "local-address"},
+    {"mailFrom": "one.reply@example.com", "rcptToLocalPart": "local"},
+    {"mailFrom": "reply@example.com", "rcptToLocalPart": "local"},
+    {"mailFrom": "reply=something@example.com", "rcptToLocalPart": "local"},
+    // These should force blob mode:
+    {"mailFrom": "reply@example.com", "rcptToLocalPart": "local=address"},
+    {"mailFrom": '"quoted string"@example.com', "rcptToLocalPart": "local"},
+    {"mailFrom": "reply@[127.0.0.1]", "rcptToLocalPart": "local"},
+];
+
+describe("test encode and decode", function () {
+    it("first set", function () {
+        testCases.forEach(testCase => {
+            // const address = `${testCase.rcptToLocalPart}@duck.com`;
+            const encRep = encodeReply(testCase, secret);
+            console.log(encRep);
+            const a = parse(`${encRep}@x.y`);
+            const decRep = decodeReply(encRep, secret);
+            assert.equal(decRep.mailFrom, testCase.mailFrom);
+            assert.equal(decRep.rcptToLocalPart, testCase.rcptToLocalPart);
+        });
     });
 });
